731cc5db396fd0ef4a036f55c4cb46a782980b14
wangzhuo <wangzhuo@yhd.com>
2016-10-29 21:31:50 +0800

    Initial Commit

---
+++ FTDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FTDemo
+//
+//  Created by iosDevelop on 16/10/29.
+//  Copyright © 2016年 tonywang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FTDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FTDemo
+//
+//  Created by iosDevelop on 16/10/29.
+//  Copyright © 2016年 tonywang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
